c6b838ff154fa3d9de294af459208ff810c517a0
sarathi247 <134366814+sarathi247@users.noreply.github.com>
2024-11-18 08:28:22 +0530

    Add files via upload

---
+++ ConditionalStatement.java
@@ -0,0 +1,26 @@
+package Allprograming;
+import java.util.Scanner;
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		// Conditional Statement programming
+		
+		Scanner obj=new Scanner(System.in);
+		String name;
+		int age;
+		
+		System.out.println("Enter your name:");
+		name=obj.nextLine();
+		System.out.println("Enter your age:");
+		age=obj.nextInt();
+		
+		if(age>=18) {
+			System.out.println("Hi "+ name+" you eligibile in your oter "+age+".");
+		}
+		else{
+			System.out.println("Hi "+name+" you not eligibile in your oter "+age+".");
+		}
+
+	}
+
+}
