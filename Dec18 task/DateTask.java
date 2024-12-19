a4c265bd4f6f2a7149e0fccaec8bb9ee7b535069
sarathi247 <134366814+sarathi247@users.noreply.github.com>
2024-12-19 14:41:23 +0530

    Create DateTask.java

---
+++ Dec18 task/DateTask.java
@@ -0,0 +1,23 @@
+package Dec19;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+public class DateTask {
+
+	public static void main(String[] args) {
+		
+		DateTimeFormatter dtf=DateTimeFormatter.ofPattern("dd//MM//yyyy");
+		
+		String format="24//07//2003";
+		LocalDate date=LocalDate.parse(format,dtf);
+	
+        LocalDate newDate = date.plusDays(4).plusMonths(4).plusYears(4);
+
+
+        System.out.println("Original date: " + date);
+        System.out.println("New date after adding 4 days, 4 months, and 4 years: " + newDate);
+		
+
+	}
+
+}
