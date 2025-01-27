87262311ee188bde50b485830791d4fc4243eeef
sarathi247 <134366814+sarathi247@users.noreply.github.com>
2025-01-27 12:24:30 +0530

    Create AllPrograming.java

---
+++ Jan27/AllPrograming.java
@@ -0,0 +1,31 @@
+ public static void prime() {
+	  
+	  int num=11;
+	  int count=0;
+	  for(int i=1;i<=num;i++) {
+		  
+		  if(num%i==0) {
+			  count++;
+		  }
+	}
+
+		   if(count==2) {
+			  System.out.println(num +" It is prime number.");
+		  }
+		  
+		  else {
+			  System.out.println(num +" It is not prime number.");
+		  }
+		    
+  }
+
+public class AllPrograming{
+  public static void main(String args[]){
+      prime();
+  }
+}
+
+
+
+
+
